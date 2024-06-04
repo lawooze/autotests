463c3d8a15042b6e3d103e8b2bce5c3fc6d161c8
lawooze <antoniod44@mail.ru>
2024-06-05 01:47:44 +0300

    Izmeneniya

--- TRPOlaba/Program.cs
+++ TRPOlaba/Program.cs
@@ -11,7 +11,7 @@ namespace TRPOlaba
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World");
+            Console.WriteLine("Hello World!!!!!!");
         }
     }
 }
